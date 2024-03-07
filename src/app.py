533b6b5b878e248a48f39208f278cdb6c9ac50c2
aestradap <55397157+aestradap@users.noreply.github.com>
2024-03-07 01:54:37 +0000

    complete first api Flask-Python

--- src/app.py
+++ src/app.py
@@ -1,5 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask('FirstFlaskApp')
+
+# @app.route('/todos', methods=['GET'])
+# def hello_world():
+#     return "<h1>Hello!</h1>"
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    # You can convert that variable into a json string like this
+    json_text = jsonify(todos)
+
+    # And then you can return it to the front end in the response body like this
+    return json_text
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    # request_body = request.get_json(force=True)
+    request_body = request.json
+    todos.append(request_body)
+   
+    return jsonify(todos)
+   
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete:", position)
+    todos.pop(position - 1)
+    return jsonify(todos)
 
 
 # These two lines should always be at the end of your app.py file
